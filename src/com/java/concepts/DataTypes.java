7032433540b318f0fcd97e5a7c3d520a8e32a269
Akshay Admane <akshay.ad@saviant.local>
2024-05-08 19:38:48 +0530

    updated DataTypes code

--- src/com/java/concepts/DataTypes.java
+++ src/com/java/concepts/DataTypes.java
@@ -9,13 +9,15 @@ public class DataTypes {
 		 * a. Boolean-true or false 
 		 * b. Numeric- i. character type- char 
 		 *             ii. Integral value-
-		 * Integers- byte, short, interger & long floating- float & double
+		 * Integers- byte, short, interger & long 
+		 * floating- float & double
 		 * character- char
 		 */
 		
 
 		/*
-		 * 1. Byte Size= 1 byte range= -128 to 127
+		 * 1. Byte Size= 1 byte 
+		 * range= -128 to 127
 		 */
 		byte b = 10;
 		byte c = 20;
@@ -24,14 +26,16 @@ public class DataTypes {
 		//===========================================================================================//
 		
 		/*
-		 * 2. Short Size= 2 bytes= 16 bits range= -32768 to 32767
+		 * 2. Short Size= 2 bytes= 16 bits 
+		 * range= -32768 to 32767
 		 */
 		short s = 129;
 		short s2 = -129;
 		//===========================================================================================//
 		
 		/*
-		 * 3. int size= 4 bytes= 32 bits range=-2147483648 to 2147483647 i.e -2^31 to
+		 * 3. int size= 4 bytes= 32 bits 
+		 * range=-2147483648 to 2147483647 i.e -2^31 to
 		 * 2^31-1
 		 */
 		int i = 2147483647;
@@ -39,8 +43,8 @@ public class DataTypes {
 		//===========================================================================================//
 
 		/*
-		 * 4. long size= 8 bytes= 64 bits range=-9,223,372,036,854,775,808 to
-		 * 9,223,372,036,854,775,807 i.e -2^63 to 2^63-1
+		 * 4. long size= 8 bytes= 64 bits 
+		 * range=-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 i.e -2^63 to 2^63-1
 		 */
 		long l = 2147483;
 		long l2 = 3423412;
@@ -52,7 +56,7 @@ public class DataTypes {
 		
 		/*
 		 * 5. float 
-		 * size= 4 bytes= 32 bits \
+		 * size= 4 bytes= 32 bits 
 		 * range after decimal-  it can take 7 digits after decimal
 		 */
 		float f1 = 12.33f;
@@ -70,15 +74,14 @@ public class DataTypes {
 		double d1 = 12.33;
 		double d2 = 12.35;
 		double d3 = 100.00; // 100.00
-		//int i4= 100.43;  gives error as float/double can save both float and interger values but integer can not
-		// save float value
+		//int i4= 100.43;  gives error as float/double can save both float and interger values but integer can notsave float value
 		// long lon2= 9223372036854775808l; // gives error as value is out of memory
 		//===========================================================================================//
 		
 		/*
 		 * 7. char
 		 * size= 2 bytes= 16 bits
-		 * char isalways declared in single quotes
+		 * char is always declared in single quotes
 		 * char accepts only single character
 		 * Range= a to z, A to Z, 0 to 9, all special characters $, @ etc
 		 */
@@ -96,7 +99,7 @@ public class DataTypes {
 		 */
 		boolean b1= true;
 		boolean b2= false; 
-		//boolean true= false; gives error
+		
 		//===========================================================================================//
 	
 	}
